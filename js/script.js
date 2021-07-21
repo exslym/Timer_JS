f2d744d3bd16fdd96c9b182e8689ff5e36a9f6cf
exslym <76844562+exslym@users.noreply.github.com>
2021-07-21 22:43:45 +0300

    add function addZero to hours, minutes, seconds

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 // Timer:
-    let deadline = '2021-08-22'; // Задаем конечную дату (дедлайн).
+    let deadline = '2022-07-20'; // Задаем конечную дату (дедлайн).
 
     function getTimeRemaining(endtime) {
         // Расчёт оставшегося времени:
@@ -10,16 +10,7 @@ window.addEventListener('DOMContentLoaded', function() {
             m = Math.floor((t/1000/60) % 60),
             h = Math.floor((t/1000/60/60) % 24),
             d = Math.floor(t/(1000*60*60*24)) + " d";
-        // Добавляем нули в отображение часов, минут, секунд в таймере:
-        if (h < 10) {
-            h = "0" + h;
-        }
-        if (m < 10) {
-            m = "0" + m;
-        }
-        if (s < 10) {
-            s = "0" + s;
-        }
+        
         // Возвращаем полученные значения:
         return {
             'total' : t,
@@ -42,14 +33,26 @@ window.addEventListener('DOMContentLoaded', function() {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
+        // Добавляем нули в отображение часов, минут, секунд в таймере:
+            function addZero(num) {
+                if (num < 10) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }            
         // Прописываем в таймер значения, получаемые ежесекундно:
-                days.textContent = t.days;
-                hours.textContent = t.hours;
-                minutes.textContent = t.minutes;
-                seconds.textContent = t.seconds;
+            days.textContent = t.days;
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
         // Если время вышло - обновляем таймер:
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }   
         }
     }
